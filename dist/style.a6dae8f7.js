4d8ade28e1e7a9afd97f5f4d8b9ffb77f94b5ebc
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-04-12 21:49:08 +0200

    fix: adding missing alt tags

--- dist/style.a6dae8f7.js
+++ dist/style.a6dae8f7.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\assets\\mobile\\image-footer.jpg":[["image-footer.b654a21e.jpg","assets/mobile/image-footer.jpg"],"assets/mobile/image-footer.jpg"],"./..\\assets\\tablet\\image-footer.jpg":[["image-footer.6704c723.jpg","assets/tablet/image-footer.jpg"],"assets/tablet/image-footer.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\assets\\mobile\\image-footer.jpg":[["image-footer.b654a21e.jpg","assets/mobile/image-footer.jpg"],"assets/mobile/image-footer.jpg"],"./..\\assets\\tablet\\image-footer.jpg":[["image-footer.6704c723.jpg","assets/tablet/image-footer.jpg"],"assets/tablet/image-footer.jpg"],"./..\\assets\\desktop\\image-footer.jpg":[["image-footer.7b36653d.jpg","assets/desktop/image-footer.jpg"],"assets/desktop/image-footer.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49906" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50059" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
